48e5158812c85fc30e4d862fd47ff2b32eb1dd3c
ItsHolmesy <matt@yellowtwist.net>
2026-03-13 22:52:43 +1100

    More updates

--- scripts/playercharacter/movement.gd
+++ scripts/playercharacter/movement.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
 @export var thrust_force: float = 2000.0
-@export var strafe_force: float = 1500.0
 
 func _ready():
 	gravity_scale = 0.0
@@ -10,27 +9,20 @@ func _ready():
 
 func get_mouse_orientation():
 	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - global_position).normalized()
-	var strafe = direction.rotated(PI / 2) # So the idea is that it grabs each direction to left and right. Which are strafe directions, the only issue is - this is tank control-like....
-	return {"direction": direction, "strafe": strafe}
+	return (mouse_pos - global_position).normalized()
 
 func _physics_process(delta):
-	var orient = get_mouse_orientation()
-	var direction = orient["direction"]
-	var strafe = orient["strafe"]
+	var direction = get_mouse_orientation()
 	
 	# Rotate to face mouse (visual only, instant... Might want to add a delay?))
 	rotation = direction.angle()
 	
 	# THRUSTING MAKES ME FEEL GOOD
-	if Input.is_action_pressed("ui_up"):
-		apply_central_force(direction * thrust_force)
-		
-	if Input.is_action_pressed("ui_down"):
-		apply_central_force(-direction * thrust_force)
-		
-	# STRAFING MAKE ME - feel not as good, but - FEEL GOOD
-	if Input.is_action_pressed("ui_left"):
-		apply_central_force(-strafe * strafe_force)
-	if Input.is_action_pressed("ui_right"):
-		apply_central_force(strafe * strafe_force)
+	if Input.is_action_pressed("moveup"):
+		apply_central_force(Vector2.UP * thrust_force)
+	if Input.is_action_pressed("movedown"):
+		apply_central_force(Vector2.DOWN * thrust_force)
+	if Input.is_action_pressed("moveleft"):
+		apply_central_force(Vector2.LEFT * thrust_force)
+	if Input.is_action_pressed("moveright"):
+		apply_central_force(Vector2.RIGHT * thrust_force)
