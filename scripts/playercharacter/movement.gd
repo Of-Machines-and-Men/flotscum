7a65dc2eb0f917885ed243aa99cf2e76d1ac6d4e
ItsHolmesy <matt@yellowtwist.net>
2026-03-13 21:02:47 +1100

    Created Character movement
    May need refinement. Also needs to BE bigger. But for now, it moves, it turns in the direction of the mouse.
    
    Pressing the UP arrow key is movement, will change keybind to W in future. Or perhaps have it be mouse-click.

--- scripts/playercharacter/movement.gd
+++ scripts/playercharacter/movement.gd
@@ -1,20 +1,19 @@
 extends RigidBody2D
 
-@export var thrust_force: float = 400.0
-@export var torque_strength: float = 200.0
-@export var orient_to_mouse: bool = true
+@export var thrust_force: float = 2000.0
 
-func _physics_process(delta: float) -> void:
-	# --- Orientation toward mouse ---
-	if orient_to_mouse:
-		var mouse_pos = get_global_mouse_position()
-		var direction = (mouse_pos - global_position).normalized()
-		var target_angle = direction.angle()
-		# Use torque to turn toward mouse
-		var angle_diff = wrapf(target_angle - rotation, -PI, PI)
-		apply_torque(angle_diff * torque_strength)
+func _ready():
+	gravity_scale = 0.0
+	linear_damp = 2.0
+	angular_damp = 4.0
 
-	# --- Thrust in facing direction (e.g. hold W or auto-move) ---
+func _physics_process(delta):
+	var mouse_pos = get_global_mouse_position()
+	var direction = (mouse_pos - global_position).normalized()
+	
+	# Rotate to face mouse (visual only, instant)
+	rotation = direction.angle()
+	
+	# Always thrust toward mouse
 	if Input.is_action_pressed("ui_up"):
-		var force_dir = Vector2.RIGHT.rotated(rotation)
-		apply_central_force(force_dir * thrust_force)
+		apply_central_force(direction * thrust_force)
