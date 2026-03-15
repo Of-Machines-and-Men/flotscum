7026651853e670c979f37b18fa528bc4b55d550b
fiachna <fiachna.carter@gmail.com>
2026-03-15 12:00:45 +1100

    use new sprites, update collision, linting fixes

--- scenes/entities/collectible/collectible.gd
+++ scenes/entities/collectible/collectible.gd
@@ -32,5 +32,5 @@ func activate() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
