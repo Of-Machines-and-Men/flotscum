b5bfaea092f4fffbd530c08e82995a29d16d0470
fiachna <fiachna.carter@gmail.com>
2026-03-16 22:29:23 +1100

    improve player input handling

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -2,6 +2,8 @@ class_name Player
 extends Entity
 
 @export var move_force: float = 100.0
+@export var rotation_torque: float = 500.0
+@export var rotation_damping: float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +12,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
+	_rotate_towards_mouse()
 	_apply_input_force()
 	
 func _apply_input_force() -> void:
@@ -20,3 +23,10 @@ func _apply_input_force() -> void:
 	
 	if input != Vector2.ZERO:
 		apply_central_force(input * move_force)
+
+func _rotate_towards_mouse() -> void:
+	var mouse_pos = get_global_mouse_position()
+	var target_angle = global_position.direction_to(mouse_pos).angle()
+	var angle_diff = wrapf(target_angle - rotation, -PI, PI)
+	apply_torque(angle_diff * rotation_torque * mass)
+	angular_velocity *= (1.0 - rotation_damping * get_physics_process_delta_time())
