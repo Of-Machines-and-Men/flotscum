fc317ff67bfcf9004af49c014953e26b4d4c2c60
fiachna <fiachna.carter@gmail.com>
2026-03-19 14:22:22 +1100

    rename refactoring

--- scripts/managers/entity_attachment_manager.gd
+++ scripts/managers/entity_attachment_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-class AbsorptionRequest:
+class AttachmentRequest:
 	var absorber: Entity
 	var target: Entity
 	
@@ -8,15 +8,15 @@ class AbsorptionRequest:
 		absorber = a
 		target = t
 		
-var _queue: Array[AbsorptionRequest] = []
+var _queue: Array[AttachmentRequest] = []
 
-func _is_duplicate(request: AbsorptionRequest, queue_entry: AbsorptionRequest):
+func _is_duplicate(request: AttachmentRequest, queue_entry: AttachmentRequest):
 	if (request.target == queue_entry.target or request.target == queue_entry.absorber) and (request.absorber == queue_entry.target or request.absorber == queue_entry.absorber):
 		return true
 	return false
 
 func _add_queue_entry(absorber: Entity, target: Entity) -> void:
-	var request = AbsorptionRequest.new(absorber, target)
+	var request = AttachmentRequest.new(absorber, target)
 	for entry in _queue:
 		if _is_duplicate(request, entry):
 			return
@@ -28,11 +28,11 @@ func _get_cluster_root(entity: Entity) -> Node:
 		current = current.get_parent()
 	return current
 
-func request_absorb(absorber: Entity, target: Entity) -> void:
+func request_attach(absorber: Entity, target: Entity) -> void:
 	var absorber_root = _get_cluster_root(absorber)
 	var target_root = _get_cluster_root(target)
 	
-	if absorber_root == target_root or not (absorber_root.can_be_absorbed or target_root.can_be_absorbed):
+	if absorber_root == target_root or not (absorber_root.can_be_attached or target_root.can_be_attached):
 		return
 	
 	if absorber.absorber_priority >= target.absorber_priority:
@@ -47,7 +47,7 @@ func _process(_delta: float) -> void:
 		var absorber_root = _get_cluster_root(request.absorber)
 		var target_root = _get_cluster_root(request.target)
 		
-		if absorber_root == target_root or not (absorber_root.can_be_absorbed or target_root.can_be_absorbed):
+		if absorber_root == target_root or not (absorber_root.can_be_attached or target_root.can_be_attached):
 			return
 		request.target.on_impact(request.absorber)
 	_queue.clear()
