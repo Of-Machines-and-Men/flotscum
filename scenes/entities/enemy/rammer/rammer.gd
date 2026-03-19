168962c3d7204e1f4f78f74510709d6d92d45944
ItsHolmesy <matt@yellowtwist.net>
2026-03-19 18:05:27 +1100

    Removed remnants of the rammer net

--- scenes/entities/enemy/rammer/rammer.gd
+++ scenes/entities/enemy/rammer/rammer.gd
@@ -1,12 +1,6 @@
 class_name Rammer
 extends Enemy
 
-@export var max_swing_angle: float = 25.0
-@export var max_net_distance: float = 40.0 
-
-@onready var net: StaticBody2D = $"Net Attach Point/Net"
-@onready var attach_point: Marker2D = $"Net Attach Point"
-
 func _ready() -> void:
 	super._ready()
 
