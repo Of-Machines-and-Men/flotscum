aef22042d65a97c68c493fa7b3431aafc6133933
ItsHolmesy <matt@yellowtwist.net>
2026-03-21 10:20:59 +1100

    Pause Menu Do-Up and Game Over Fix.

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -3,7 +3,9 @@ extends Control
 func _on_retry_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/levels/game_world.tscn")
+	queue_free()
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/levels/UI/main_menu.tscn")
+	queue_free()
